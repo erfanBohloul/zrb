704509ac47f4cedd007839cc90f8183f9a23582b
Erfan Bohloul <erfanbohloul@gmail.com>
2024-01-12 20:38:13 +0330

    add arg to main func

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main()
+#define debug(x) printf("%s:%x\n", #x, x)
+
+int main(int argc, char **argv)
 {
     printf("Hello World!\n");
+    debug(argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%d: %s\n", i, argv[i]);
+    }
 }
