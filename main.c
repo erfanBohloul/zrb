5e1a06c543243f5c48656273689589447c718598
Erfan Bohloul <erfanbohloul@gmail.com>
2024-01-23 12:58:45 +0330

    some change in git init

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #define debug(x) printf("%s:%d\n", #x, x)
 #define debug_s(x) printf("%s:%s\n", #x, x)
 #define alias_path "/Users/erfan/Codes/zrb/alias_ha.txt"
+#define global_conf "/Users/erfan/Codes/zrb/author.txt"
 
 typedef int (*func_ptr)(int, char **);
 
@@ -26,10 +27,16 @@ char **handle_alias(int *argc, char **command);
 char *get_parent_folder(char *path);
 int count_word(char *str);
 int is_file_empty(FILE *file);
+char *get_local_config_path();
+void copy_file(FILE *src, FILE *dest);
 
 // errors:
 void Undefined_Behaviour();
 void Invalid_Command();
+void Not_repo();
+
+// config
+int change_config(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
@@ -46,7 +53,6 @@ int main(int argc, char **argv)
         printf("%s ", argv[i]);
     }
     printf("\n");
-    return 0;
 
     func_ptr command_func = input_finder(argc, &argv[1]);
     if (!command_func)
@@ -55,19 +61,7 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    int status = command_func(argc, &argv[1]);
-    return 0;
-}
-
-int change_config(char **argv)
-{
-    int is_it_global = 0, idx = 1;
-    if (!strcmp(argv[idx], "-global"))
-    {
-        is_it_global = 1;
-        idx++;
-    }
-
+    int status = command_func(argc - 1, &argv[1]);
     return 0;
 }
 
@@ -197,7 +191,7 @@ func_ptr input_finder(int argc, char **argv)
     // looking for command in valid commands and then return a sutable function
     if (command == NULL)
     {
-        fprintf(stderr, "[ERR] Command is null.\n");
+        fprintf(stderr, "[ERROR] Command is null.\n");
         return NULL;
     }
 
@@ -206,10 +200,139 @@ func_ptr input_finder(int argc, char **argv)
         return &create_repo;
     }
 
+    if (!strcmp(command, "config"))
+    {
+        return &change_config;
+    }
+
     // else:
     Invalid_Command();
 
     return NULL;
+}
+
+int change_config_file(FILE *conf_file, int argc, char **argv, int idx)
+{
+    // file must be in write mode
+    if (conf_file == NULL)
+    {
+        printf("Error opening config file\n");
+        return -1;
+    }
+
+    int line_num = 0;
+    char line[100] = "user.";
+    if (!strcmp(argv[idx], "user.name"))
+    {
+        strcat(line, "name = ");
+        strcat(line, argv[idx + 1]);
+        line_num = 1;
+    }
+    else if (!strcmp(argv[idx], "user.name"))
+    {
+        strcat(line, "email = ");
+        strcat(line, argv[idx + 1]);
+        line_num = 2;
+    }
+
+    else
+    {
+        Invalid_Command();
+        return -1;
+    }
+
+    // change file:
+    int count = 0;
+    char old_line[100];
+    while (fgets(old_line, sizeof line, conf_file))
+    {
+        count++;
+        if (count == line_num)
+        {
+            fprintf(conf_file, "%s", line);
+        }
+
+        fprintf(conf_file, "%s", old_line);
+    }
+
+    fclose(conf_file);
+    return 0;
+}
+
+char *get_local_config_path()
+{
+    char *repo_path = get_repo_path();
+    if (repo_path == NULL)
+    {
+        Not_repo();
+        return NULL;
+    }
+
+    strcat(repo_path, "/author.txt");
+    return strdup(repo_path);
+}
+
+int change_config(int argc, char **argv)
+{
+    char *repo_path = get_repo_path();
+    if (!repo_path)
+    {
+        Not_repo();
+        return -1;
+    }
+
+    FILE *conf_file;
+
+    int idx = 2;
+    if (!strcmp(argv[2], "-global"))
+    {
+        printf("Global configuration\n");
+        conf_file = fopen(global_conf, "w");
+        change_config_file(conf_file, argc, argv, idx + 1);
+        idx++;
+    }
+
+    printf("Local configuration\n");
+    conf_file = fopen(get_local_config_path(), "w");
+    change_config_file(conf_file, argc, argv, idx);
+
+    fclose(conf_file);
+    free(repo_path);
+
+    return 0;
+}
+
+void copy_file(FILE *src, FILE *dest)
+{
+    int ch;
+    while ((ch = fgetc(src)) != EOF)
+    {
+        fputc(ch, dest);
+    }
+}
+
+// int newCommit()
+// {
+//     // create a helper FILE
+//     FILE *tmp_w = fopen("./.zrb/help", "w");
+//     if (!tmp_w)
+//     {
+//         perror("fopen tmp_w");
+//         return -1;
+//     }
+
+//     FILE *tmp_r = fopen("./.zrb/help", "r");
+//     if (!tmp_r)
+//     {
+//         perror("fopen tmp_r");
+//         return -1;
+//     }
+// }
+
+void write_to_file(FILE *file, char *str)
+{
+    // the file is in the write mode
+    fputs(str, file);
 }
 
 int create_repo(int argc, char **argv)
@@ -226,19 +349,49 @@ int create_repo(int argc, char **argv)
         return 0;
     }
 
-    // FILE *global_config = fopen(global_conf_path, "r");
-    // if (global_conf_path == NULL)
-    // {
-    //     printf("[ERR] couldn't find the global config path\n");
-    //     return 0;
-    // }
+    printf("[SUCC] .zrb file has create\n");
 
-    // FILE *conf = fopen("conf.txt", "w+");
-    // if (global_conf_path == NULL)
-    // {
-    //     printf("[ERR] can not build a config file\n");
-    //     return 0;
-    // }
+    // handle_author
+    FILE *author = fopen("./.zrb/author.txt", "w");
+    if (!author)
+    {
+        perror("[ERROR] fopen author.txt");
+        return -1;
+    }
+    FILE *global_author = fopen(global_conf, "r");
+    if (!global_author)
+    {
+        perror("[ERROR] fopen global_author.txt");
+        return -1;
+    }
+
+    copy_file(global_author, author);
+    fclose(author);
+    fclose(global_author);
+
+    printf("[SUCC] author has set\n");
+
+    // files
+    if (mkdir("./.zrb/files", 0777) == -1)
+    {
+        perror("[ERROR] mkdir .zrb/files");
+        return -1;
+    }
+
+    // create a root commit:
+    FILE *root_commit = fopen("./.zrb/files/root.txt", "w");
+    write_to_file(root_commit, "commit root\nroot\n");
+
+    printf("[SUCC] folder of file has created\n");
+
+    // create stage folder:
+    if (mkdir("./.zrb/stage", 0777) == -1)
+    {
+        perror("[ERROR] mkdir ./.zrb/stage");
+        return -1;
+    }
+
+    printf("[SUCC] the stage has created\n");
     return 0;
 }
 
@@ -292,4 +445,9 @@ void Undefined_Behaviour()
 void Invalid_Command()
 {
     fprintf(stderr, "[ERROR] invalid command\n");
+}
+
+void Not_repo()
+{
+    fprintf(stderr, "[ERROR] not a zrb repo\n");
 }
